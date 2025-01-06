c037d168619188d0194110a159d8a4d520211438
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-01-07 00:32:58 +0500

    did some practice of asyns programming

--- Todo List/todo_list.js
+++ Todo List/todo_list.js
@@ -29,20 +29,36 @@ function renderTodoDiv() {
     let divElem = document.querySelector(".js-todos");
     divElem.innerHTML = "";
 
-    for (let i=0; i<todoArray.length; i++) {
-        divElem.innerHTML += `<div id="${i}">
+    // A short technique for looping through the array (Prefered)
+    todoArray.forEach(function(element, index) {
+        divElem.innerHTML += `<div id="${index}">
                 <div class="text">
-                    <p>${todoArray[i][0]}</p>
+                    <p>${element[0]}</p>
                 </div>
                 <div class="date">
-                    <p>${todoArray[i][1]}</p>
+                    <p>${element[1]}</p>
                 </div>
 
                 <div class="delt-button">
-                    <button class="button" onclick="deleteTodo('${i}')">Delete</button>
+                    <button class="button" onclick="deleteTodo('${index}')">Delete</button>
                 </div>
             </div>`;
-    }
+    });
+
+    // for (let i=0; i<todoArray.length; i++) {
+    //     divElem.innerHTML += `<div id="${i}">
+    //             <div class="text">
+    //                 <p>${todoArray[i][0]}</p>
+    //             </div>
+    //             <div class="date">
+    //                 <p>${todoArray[i][1]}</p>
+    //             </div>
+
+    //             <div class="delt-button">
+    //                 <button class="button" onclick="deleteTodo('${i}')">Delete</button>
+    //             </div>
+    //         </div>`;
+    // }
 }
 
 renderTodoDiv()
