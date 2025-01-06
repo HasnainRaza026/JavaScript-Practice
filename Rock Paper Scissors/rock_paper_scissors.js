eab0300f85acad9fb15cbfb280d6967dae3610fe
HasnainRaza026 <hasnainraza0026@gmail.com>
2025-01-06 22:09:50 +0500

    learned js asyncronus programming and added auto play feature in my rock paper scissors code

--- Rock Paper Scissors/rock_paper_scissors.js
+++ Rock Paper Scissors/rock_paper_scissors.js
@@ -1,6 +1,9 @@
 const picks = ["rock", "paper", "scissors"];
 let scoreTracker = JSON.parse(localStorage.getItem('score')) || { win: 0, lose: 0, tie: 0 };
 
+let intervalId;
+let isAutoPlay = false;
+
 function checkWinner(myPick) {
     const computerPick = picks[Math.floor(Math.random() * picks.length)];
 
@@ -33,8 +36,28 @@ function checkWinner(myPick) {
     localStorage.setItem('score', JSON.stringify(scoreTracker));
 }
 
+
 function resetScore() {
     scoreTracker = { win: 0, lose: 0, tie: 0 };
     document.querySelector(".js-score").innerText = `Wins: ${scoreTracker.win}, Losses: ${scoreTracker.lose}, Ties: ${scoreTracker.tie}`;
     localStorage.setItem('score', JSON.stringify(scoreTracker));
+}
+
+
+function autoPlay() {
+    buttonElem = document.querySelector(".js-autoplay");
+
+    if (isAutoPlay) {
+        isAutoPlay = false;
+        buttonElem.innerText = "Auto Play";
+        clearInterval(intervalId);
+        return
+    }
+
+    isAutoPlay = true;
+    buttonElem.innerText = "Stop Play";
+    intervalId = setInterval(function play() {
+        const pick1 = picks[Math.floor(Math.random() * picks.length)];
+        checkWinner(myPick=pick1);
+    }, 1000)
 }
